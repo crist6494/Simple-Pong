eeee5babed82ef97389dbaffe851738a4e1ce20a
cmorales <moralesrojascr@gmail.com>
2024-05-15 00:50:50 +0200

    Basic game done, can upgrade more

--- js/elements.js
+++ js/elements.js
@@ -1,3 +1,5 @@
+"use strict"
+
 //Return a objet (x,y)
 function vec2(x, y)
 {
@@ -10,10 +12,10 @@ class Ball
     constructor()
     {
         this.pos = {x: getWidth() / 2, y: getHeight() / 2};
-        this.velocity = {x: 4,  y: 0};
+        this.velocity = {x: getWidth() * 0.003,  y: 0};
         this.radius = 0.006 * getWidth();
         this.maxAngle = 55;
-        this.speed = 5;
+        this.speed = this.velocity.x;
         this.deltaTime = 0;
     }
 
@@ -27,21 +29,21 @@ class Ball
     render()
     {
         pong.ctx.beginPath();
-        pong.ctx.fillStyle = "#33ff00";
-        pong.ctx.strokeStyle = "#33ff00";
+        pong.ctx.fillStyle = "magenta";
+        //pong.ctx.strokeStyle = "#33ff00";
         pong.ctx.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
         pong.ctx.fill();
-        pong.ctx.stroke();
+        //pong.ctx.stroke();
     };
 
     respawnBall()
     {
         this.pos.x = getWidth() / 2;
         this.pos.y = getHeight() / 2;
-        this.reset_velocity = {x: 4,  y: 0};
+        this.reset_velocity = {x: getWidth() * 0.003,  y: 0};
         this.velocity.x = this.reset_velocity.x;
         this.velocity.y = this.reset_velocity.y;
-        this.speed = 5;
+        this.speed = this.velocity.x;
     }
 }
 
@@ -49,11 +51,12 @@ class Ball
 //Class for the paddles
 class Paddle
 {
-    constructor(pos, width, height)
+    constructor(pose, width, height)
     {
         this.score = 0;
-        this.pos = pos; //vec2
-        this.velocity = {x: 0, y: 6}; //vec2
+        this.pos = pose; //vec2
+        this.init_pos = {x: pose.x, y: pose.y};
+        this.velocity = {x: 0, y: getWidth() * 0.004}; //vec2
         this.width = width;
         this.height = height;
         this.speed = 5;
@@ -62,7 +65,7 @@ class Paddle
 
     render()
     {
-        pong.ctx.fillStyle = "#33ff00";
+        pong.ctx.fillStyle = "cyan";
         pong.ctx.fillRect(this.pos.x, this.pos.y, this.width, this.height);
     };
 
@@ -83,24 +86,22 @@ class Paddle
 
     moveUp ()
     {
-        if(this.pos.y > 0)
+        const pitchTop = 0 + pong.margin + pong.lineWidth;
+        if(this.pos.y > pitchTop)
             this.pos.y -= this.velocity.y;
     }
 
     moveDown ()
     {
-        if(this.pos.y + this.height < canvas.height)
+        const pitchBottom = canvas.height - pong.margin - pong.lineWidth;
+        if(this.pos.y + this.height < pitchBottom)
             this.pos.y += this.velocity.y;
     }
 
     reset()
     {
-        this.score = 0;
-        this.pos = pos; //vec2
-        this.velocity = {x: 0, y: 6}; //vec2
-        this.width = width;
-        this.height = height;
-        this.speed = 5;
+        this.pos = { ...this.init_pos };//Assignment obj => variables = reference with ... create a deep copy
+        //this.pos = Object.assign({}, this.init_pos); //Other option
     }
 }
 
@@ -110,7 +111,7 @@ function ballCollisionEdges(ball)
     const top = ball.pos.y - ball.radius;
     //const left = ball.pos.x - ball.radius;
     //const right = ball.pos.x + ball.radius;
-    if((bottom > canvas.height && ball.velocity.y > 0) || (top < 0 && ball.velocity.y < 0))
+    if((bottom > canvas.height - pong.margin - pong.lineWidth && ball.velocity.y > 0) || (top < 0 + pong.margin + pong.lineWidth && ball.velocity.y < 0))
         ball.velocity.y *= -1;
 
     /* if(right >= canvas.width /*|| left <= 0)
@@ -135,6 +136,7 @@ function gradeToRadians(angle)
 
 function paddleBouncedBall(ball, paddle, sign)
 {
+    console.log(getWidth());
     let dy = ball.pos.y - paddle.getCenterPaddle().y; //Difference .y ball .y paddleCenter to know direction
     let angle = (ball.maxAngle * dy) / (paddle.height / 2); //Divide paddleHEight normalize the result to = [-1, 0, 1]
     angle = gradeToRadians(angle);
@@ -142,26 +144,56 @@ function paddleBouncedBall(ball, paddle, sign)
         ball.velocity.x = -Math.abs(ball.speed * Math.cos(angle));
     else
         ball.velocity.x = Math.abs(ball.speed * Math.cos(angle));
-    ball.velocity.y = ball.speed * Math.sin(angle); 
-    if(ball.speed < 12)
-        ball.speed += 0.4;
+    ball.velocity.y = ball.speed * Math.sin(angle);
+
+    if(getWidth() >= 1920)
+    {
+        if(ball.speed < 14)
+            ball.speed += 0.6;
+        else
+            ball.speed = 14
+    }
+    else if(getWidth() >= 1300)
+    {
+        if(ball.speed < 10)
+            ball.speed += 0.5;
+        else
+            ball.speed = 10
+    }
+    else if(getWidth() >= 800)
+    {
+        if(ball.speed < 8)
+            ball.speed += 0.3;
+        else
+            ball.speed = 5
+    }
+    else
+    {
+        if(ball.speed < 4)
+            ball.speed += 0.3;
+        else
+            ball.speed = 5
+        console.log(paddle.velocity.y);
+        paddle.velocity.y += 0.005;
+    }
+    
 }
 
 function ballPaddleCollision(ball, paddle)
 {
-    if (ball.pos.x - ball.radius <= paddle.pos.x + paddle.width && // Edge rigth, left paddle <===> left ball side
-        ball.pos.x - ball.radius >= paddle.pos.x && // Edge left, left paddle <===> left ball side (*corner*)
-        ball.pos.y + ball.radius >= paddle.pos.y && // Edge top ,left paddle <===> bottom side ball
-        ball.pos.y - ball.radius <= paddle.pos.y + paddle.height &&  // Edge top ,left paddle <===> bottom side ball
+    if (ball.pos.x - ball.radius < paddle.pos.x + paddle.width && // Edge rigth, left paddle <===> left ball side
+        ball.pos.x - ball.radius > paddle.pos.x && // Edge left, left paddle <===> left ball side (*corner*)
+        ball.pos.y + ball.radius > paddle.pos.y && // Edge top ,left paddle <===> bottom side ball
+        ball.pos.y - ball.radius < paddle.pos.y + paddle.height &&  // Edge top ,left paddle <===> bottom side ball
         ball.velocity.x < 0) //Checked left paddle
     {
         paddleBouncedBall(ball, paddle, 1)
     }
     
-    if (ball.pos.x + ball.radius <= paddle.pos.x + paddle.width && //Edge rigth, right paddle <===> right ball side (*corner*)
-        ball.pos.x + ball.radius >= paddle.pos.x && // Edge left, right paddle <===> right ball side (*corner*)
-        ball.pos.y + ball.radius >= paddle.pos.y && // Edge top ,right paddle <===> bottom side ball
-        ball.pos.y - ball.radius <= paddle.pos.y + paddle.height && // Edge top ,left paddle <===> bottom side ball
+    if (ball.pos.x + ball.radius < paddle.pos.x + paddle.width && //Edge rigth, right paddle <===> right ball side (*corner*)
+        ball.pos.x + ball.radius > paddle.pos.x && // Edge left, right paddle <===> right ball side (*corner*)
+        ball.pos.y + ball.radius > paddle.pos.y && // Edge top ,right paddle <===> bottom side ball
+        ball.pos.y - ball.radius < paddle.pos.y + paddle.height && // Edge top ,left paddle <===> bottom side ball
         ball.velocity.x > 0) //Checked right paddle
     {
         paddleBouncedBall(ball, paddle, -1)
@@ -171,19 +203,23 @@ function ballPaddleCollision(ball, paddle)
 
 function upScore(ball, paddle1, paddle2)
 {
-    if(ball.pos.x <= -ball.radius)
+    if(ball.pos.x <= 0 + pong.margin)
     {
         paddle2.score++;
         document.querySelector(".player2--score").innerHTML = paddle2.score; //Modified the HTML content
         ball.respawnBall();
+        paddle1.reset();
+        paddle2.reset();
         ball.velocity.x = -Math.abs(ball.velocity.x);
     }
 
-    if(ball.pos.x >= canvas.width + ball.radius)
+    if(ball.pos.x >= canvas.width - pong.margin + ball.radius)
     {
         paddle1.score++;
         document.querySelector(".player1--score").innerHTML = paddle1.score; //Modified the HTML content
         ball.respawnBall();
+        paddle1.reset();
+        paddle2.reset();
         ball.velocity.x = Math.abs(ball.velocity.x);
     }
 }
--- js/pong.js
+++ js/pong.js
@@ -26,6 +26,10 @@ class Pong
         this.canvas.width = getWidth();
         this.canvas.height= getHeight();
 
+        this.margin = 42;
+        this.pitchWidth = this.canvas.width - this.margin * 2;
+        this.pitchHeight = this.canvas.height - this.margin * 2;
+
         this.ctx = canvas.getContext("2d"); // the rederezing context
 
         this.ball = new Ball();
@@ -35,57 +39,55 @@ class Pong
     
     createPaddles()
     {
-        this.paddleHeight = (getHeight()) / 2;
-        const paddleWidth = 0.01 * getWidth();
-        const paddleHeight = 0.16 * getHeight();
-        const paddle2x = this.canvas.width - (0.05 * getWidth()) - paddleWidth;
+        const paddleWidth = 0.0085* getWidth();
+        const paddleHeight = 0.15 * getHeight();
+    
+        const paddle2x = this.pitchWidth - (0.008 * this.pitchWidth) - paddleWidth;
         const centerY = 0.5 * this.canvas.height - (paddleHeight / 2);
 
-        this.paddle1 = new Paddle(vec2(getWidth() * 0.05, centerY), paddleWidth, paddleHeight);
+
+        this.paddle1 = new Paddle(vec2(this.pitchWidth * 0.008 + this.margin * 2, centerY), paddleWidth, paddleHeight);
         this.paddle2 = new Paddle(vec2(paddle2x, centerY), paddleWidth, paddleHeight);
     }
 
     drawFieldLines() {
-        const fieldWidth = this.canvas.width - 100;
-        const fieldHeight = this.canvas.height - 100;
-        const margin = 50; 
-        const lineWidth = 3;
-        const dashLength = 30; //height discontinued line
+       
+        this.lineWidth = 4;
+        const dashLength = 25; //height discontinued line
     
-
-        this.ctx.strokeStyle = "white"; // Line color
-        this.ctx.lineWidth = lineWidth; // Width line
+        this.ctx.strokeStyle = "magenta"; // Line color
+        this.ctx.lineWidth = this.lineWidth; // Width line
     
         // Top line
-        // this.ctx.beginPath();
-        // this.ctx.moveTo(margin, margin);
-        // this.ctx.lineTo(margin + fieldWidth, margin);
-        // this.ctx.stroke();
-    
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.margin - this.lineWidth / 2, this.margin);
+        this.ctx.lineTo(this.margin + this.pitchWidth + this.lineWidth / 2, this.margin);
+        this.ctx.stroke();
         // Bottom line
-        // this.ctx.beginPath();
-        // this.ctx.moveTo(margin, margin + fieldHeight);
-        // this.ctx.lineTo(margin + fieldWidth, margin + fieldHeight);
-        // this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.margin - this.lineWidth / 2, this.margin + this.pitchHeight);
+        this.ctx.lineTo(this.margin + this.pitchWidth + this.lineWidth / 2, this.margin + this.pitchHeight );
+        this.ctx.stroke();
     
         // Left line
-        // this.ctx.beginPath();
-        // this.ctx.moveTo(margin, margin);
-        // this.ctx.lineTo(margin, margin + fieldHeight);
-        // this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.margin, this.margin);
+        this.ctx.lineTo(this.margin, this.margin + this.pitchHeight);
+        this.ctx.stroke();
     
         // Right line
-        // this.ctx.beginPath();
-        // this.ctx.moveTo(margin + fieldWidth, margin);
-        // this.ctx.lineTo(margin + fieldWidth, margin + fieldHeight);
-        // this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.margin + this.pitchWidth, this.margin);
+        this.ctx.lineTo(this.margin + this.pitchWidth, this.margin + this.pitchHeight);
+        this.ctx.stroke();
 
         // Draw discontinued line in the mid fields
         this.ctx.setLineDash([dashLength, dashLength]); // Establecer el patrón de línea discontinua
+        this.ctx.strokeStyle = "white"; // Line color
         // Vertical line field
         this.ctx.beginPath();
-        this.ctx.moveTo(margin + fieldWidth / 2, 0);
-        this.ctx.lineTo(margin + fieldWidth / 2, this.canvas.height);
+        this.ctx.moveTo(this.margin + this.pitchWidth / 2, this.margin + this.lineWidth);
+        this.ctx.lineTo(this.margin + this.pitchWidth / 2, this.margin + this.pitchHeight - this.lineWidth);
         this.ctx.stroke();
     
         // Restart continuous pattern
@@ -96,6 +98,8 @@ class Pong
 
         this.canvas.width = getWidth();
         this.canvas.height = getHeight();
+        this.pitchWidth = this.canvas.width - this.margin * 2;
+        this.pitchHeight = this.canvas.height - this.margin * 2;
         this.ball.radius = 0.006 * getWidth(); //Sie ball
         this.createPaddles(); //Size Paddles
     }
@@ -104,19 +108,18 @@ class Pong
     {
         //() => this.gameLoop() arrow function call the function
         window.requestAnimationFrame(() => this.gameLoop()); //Call the (function) befeore the next repaitn of the browser 60 times per second
-        let currenTime = performance.now();
-        const deltaTime = (currenTime - this.time) / 1000;
-        currenTime = this.time;
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); //Restart the select pixels => coords(x,y) and size(width, height)
         //ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
         //ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        gameUpdate(this.ball, this.paddle1, this.paddle2, deltaTime);
+        gameUpdate(this.ball, this.paddle1, this.paddle2);
         gameDraw(this.ball, this.paddle1, this.paddle2);
     }
 };
 
-function gameUpdate(ball, paddle1, paddle2, deltaTime)
+
+
+function gameUpdate(ball, paddle1, paddle2)
 {
     ball.move()
     move2(paddle1);
@@ -130,7 +133,6 @@ function gameUpdate(ball, paddle1, paddle2, deltaTime)
     ballPaddleCollision(ball, paddle2);
     //swplayer2IA(paddle2, ball);
         
-    upScore(ball, paddle1, paddle2)
 }
 
 function gameDraw(ball, paddle1, paddle2)
@@ -139,6 +141,7 @@ function gameDraw(ball, paddle1, paddle2)
     ball.render();
     paddle1.render();
     paddle2.render();
+    upScore(ball, paddle1, paddle2)
 }
 
 
@@ -193,6 +196,8 @@ function player2IA(paddle, ball)
         }
     }
 } 
+
+
 
 
 //Start game
